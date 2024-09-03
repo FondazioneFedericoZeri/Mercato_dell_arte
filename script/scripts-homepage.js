41239c0bc15ad0254c3b85374398fac530ecfa15
giuliarenda <giuliarenda94@gmail.com>
2024-09-03 11:45:39 +0200

    tooltip mappa con indirizzo completo

--- script/scripts-homepage.js
+++ script/scripts-homepage.js
@@ -9,87 +9,87 @@ $.getJSON("https://raw.githubusercontent.com/FondazioneFedericoZeri/Mercato_dell
 });
 
 // scripts js sezione 2 'search' che permette di applicare la dissolvenza in entrata al testo allo scroll della pagina
-document.addEventListener("DOMContentLoaded", function() {
-    const searchSection = document.querySelector('#search');
+document.addEventListener("DOMContentLoaded", function () {
+  const searchSection = document.querySelector('#search');
 
-    function checkVisibility() {
-        const rect = searchSection.getBoundingClientRect();
-        if (rect.top < window.innerHeight && rect.bottom >= 0) {
-            searchSection.classList.add('visible');
-        }
+  function checkVisibility() {
+    const rect = searchSection.getBoundingClientRect();
+    if (rect.top < window.innerHeight && rect.bottom >= 0) {
+      searchSection.classList.add('visible');
     }
+  }
 
-    window.addEventListener('scroll', checkVisibility);
-    window.addEventListener('resize', checkVisibility);
+  window.addEventListener('scroll', checkVisibility);
+  window.addEventListener('resize', checkVisibility);
 
-    // Controlla la visibilità al caricamento della pagina
-    checkVisibility();
+  // Controlla la visibilità al caricamento della pagina
+  checkVisibility();
 });
 
 
- // scripts js sezione 3: permette di incrementare i numeri
+// scripts js sezione 3: permette di incrementare i numeri
 
-document.addEventListener("DOMContentLoaded", function() {
-    function animateValue(obj, start, end, duration) {
-        let startTimestamp = null;
-        const step = (timestamp) => {
-            if (!startTimestamp) startTimestamp = timestamp;
-            const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-            obj.innerHTML = Math.floor(progress * (end - start) + start);
-            if (progress < 1) {
-                window.requestAnimationFrame(step);
-            } else {
-                obj.classList.add('bold'); // Aggiungi la classe per il grassetto dopo l'animazione
-            }
-        };
+document.addEventListener("DOMContentLoaded", function () {
+  function animateValue(obj, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      obj.innerHTML = Math.floor(progress * (end - start) + start);
+      if (progress < 1) {
         window.requestAnimationFrame(step);
+      } else {
+        obj.classList.add('bold'); // Aggiungi la classe per il grassetto dopo l'animazione
+      }
+    };
+    window.requestAnimationFrame(step);
+  }
+
+  function checkStatisticsVisibility() {
+    const statsSection = document.querySelector("#statistics");
+    const rect = statsSection.getBoundingClientRect();
+    if (rect.top < window.innerHeight && rect.bottom >= 0 && !statsSection.classList.contains('animated')) {
+      statsSection.classList.add('animated');
+      const stats = document.querySelectorAll("#statistics .stat h3");
+      stats.forEach(stat => {
+        const endValue = parseInt(stat.innerHTML, 10);
+        stat.innerHTML = "0";
+        animateValue(stat, 0, endValue, 2000);
+      });
     }
+  }
 
-    function checkStatisticsVisibility() {
-        const statsSection = document.querySelector("#statistics");
-        const rect = statsSection.getBoundingClientRect();
-        if (rect.top < window.innerHeight && rect.bottom >= 0 && !statsSection.classList.contains('animated')) {
-            statsSection.classList.add('animated');
-            const stats = document.querySelectorAll("#statistics .stat h3");
-            stats.forEach(stat => {
-                const endValue = parseInt(stat.innerHTML, 10);
-                stat.innerHTML = "0";
-                animateValue(stat, 0, endValue, 2000);
-            });
-        }
-    }
+  window.addEventListener('scroll', checkStatisticsVisibility);
+  window.addEventListener('resize', checkStatisticsVisibility);
 
-    window.addEventListener('scroll', checkStatisticsVisibility);
-    window.addEventListener('resize', checkStatisticsVisibility);
-
-    // Controlla la visibilità al caricamento della pagina
-    checkStatisticsVisibility();
+  // Controlla la visibilità al caricamento della pagina
+  checkStatisticsVisibility();
 });
 
 
- // scripts js sezione 4 map: permette di applicare la dissolvenza in entrata al testo allo scroll della pagina
-document.addEventListener("DOMContentLoaded", function() {
-    const mapTextContent = document.querySelector('#map .text-content');
+// scripts js sezione 4 map: permette di applicare la dissolvenza in entrata al testo allo scroll della pagina
+document.addEventListener("DOMContentLoaded", function () {
+  const mapTextContent = document.querySelector('#map .text-content');
 
-    function checkMapVisibility() {
-        const rect = mapTextContent.getBoundingClientRect();
-        if (rect.top < window.innerHeight && rect.bottom >= 0) {
-            mapTextContent.classList.add('visible');
-        }
+  function checkMapVisibility() {
+    const rect = mapTextContent.getBoundingClientRect();
+    if (rect.top < window.innerHeight && rect.bottom >= 0) {
+      mapTextContent.classList.add('visible');
     }
+  }
 
-    window.addEventListener('scroll', checkMapVisibility);
-    window.addEventListener('resize', checkMapVisibility);
+  window.addEventListener('scroll', checkMapVisibility);
+  window.addEventListener('resize', checkMapVisibility);
 
-    // Controlla la visibilità al caricamento della pagina
-    checkMapVisibility();
+  // Controlla la visibilità al caricamento della pagina
+  checkMapVisibility();
 });
 
 
 
 
- // scripts js sezione 4: mappa interattiva con locatoot
- am5.ready(function() {
+// scripts js sezione 4: mappa interattiva con locatoot
+am5.ready(function () {
 
   // Create root element
   // https://www.amcharts.com/docs/v5/getting-started/#Root_element
@@ -125,7 +125,7 @@ document.addEventListener("DOMContentLoaded", function() {
   );
 
   polygonSeries.mapPolygons.template.setAll({
-    fill:am5.color(0xdadada)
+    fill: am5.color(0xdadada)
   });
 
 
@@ -136,9 +136,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // Set clustered bullet
   // https://www.amcharts.com/docs/v5/charts/map-chart/clustered-point-series/#Group_bullet
-  pointSeries.set("clusteredBullet", function(root) {
+  pointSeries.set("clusteredBullet", function (root) {
     var container = am5.Container.new(root, {
-      cursorOverStyle:"pointer"
+      cursorOverStyle: "pointer"
     });
 
     var circle1 = container.children.push(am5.Circle.new(root, {
@@ -170,7 +170,7 @@ document.addEventListener("DOMContentLoaded", function() {
       text: "{value}"
     }));
 
-    container.events.on("click", function(e) {
+    container.events.on("click", function (e) {
       pointSeries.zoomToCluster(e.target.dataItem);
     });
 
@@ -180,7 +180,7 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   // Create regular bullets
-  pointSeries.bullets.push(function() {
+  pointSeries.bullets.push(function () {
     var circle = am5.Circle.new(root, {
       radius: 6,
       tooltipY: 0,
@@ -199,9 +199,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
   console.log(luoghi_json);
 
-  for (let luogo in luoghi_json){
-    if (luoghi_json[luogo]["geo"]["lat"]){
-      city = {title: luoghi_json[luogo]["Città"],
+  for (let luogo in luoghi_json) {
+    if (luoghi_json[luogo]["geo"]["lat"]) {
+      place_name = luoghi_json[luogo]["Città"]
+      if (luoghi_json[luogo]["Via"].length > 0) {
+        place_name = place_name.concat(', ', luoghi_json[luogo]["Via"])
+        if (luoghi_json[luogo]["Civico"].length > 0) {
+          place_name = place_name.concat(', ', luoghi_json[luogo]["Civico"])
+        }
+      }
+      city = {
+        title: place_name,
         latitude: luoghi_json[luogo]["geo"]["lat"],
         longitude: luoghi_json[luogo]["geo"]["lon"],
       };
@@ -225,34 +233,34 @@ document.addEventListener("DOMContentLoaded", function() {
   // Make stuff animate on load
   chart.appear(1000, 100);
 
-  }); // end am5.ready()
+}); // end am5.ready()
 
 
- // scripts js sezione 5 timeline: permette di applicare la dissolvenza in entrata al testo allo scroll della pagina
- document.addEventListener("DOMContentLoaded", function() {
-    // Funzione per controllare se un elemento è visibile nello schermo
-    function isElementInViewport(el) {
-        var rect = el.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    }
+// scripts js sezione 5 timeline: permette di applicare la dissolvenza in entrata al testo allo scroll della pagina
+document.addEventListener("DOMContentLoaded", function () {
+  // Funzione per controllare se un elemento è visibile nello schermo
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
 
-    // Funzione per applicare l'effetto di dissolvenza
-    function checkFadeIn() {
-        var elements = document.querySelectorAll('.fade-in');
-        elements.forEach(function(element) {
-            if (isElementInViewport(element)) {
-                element.classList.add('visible');
-            }
-        });
-    }
+  // Funzione per applicare l'effetto di dissolvenza
+  function checkFadeIn() {
+    var elements = document.querySelectorAll('.fade-in');
+    elements.forEach(function (element) {
+      if (isElementInViewport(element)) {
+        element.classList.add('visible');
+      }
+    });
+  }
 
-    // Controlla lo scroll e carica
-    window.addEventListener('scroll', checkFadeIn);
-    window.addEventListener('load', checkFadeIn);
+  // Controlla lo scroll e carica
+  window.addEventListener('scroll', checkFadeIn);
+  window.addEventListener('load', checkFadeIn);
 });
 
